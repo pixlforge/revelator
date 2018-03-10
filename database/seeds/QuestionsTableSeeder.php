f23f3f39288729a5f3f2212c064955f5b8318fc7
Pixlforge <celien@pixlforge.ch>
2018-03-10 13:40:45 +0100

    Replaced basic names by the real ones

--- database/seeds/QuestionsTableSeeder.php
+++ database/seeds/QuestionsTableSeeder.php
@@ -12,85 +12,85 @@ class QuestionsTableSeeder extends Seeder
     public function run()
     {
         factory(\App\Question::class)->create([
-            'name' => 'Birthdate',
+            'name' => "I'm in the age range...",
             'type' => 'dropdown',
             'pos' => 0
         ]);
 
         factory(\App\Question::class)->create([
-            'name' => 'Gender',
+            'name' => "I am a...",
             'type' => 'multiple',
             'pos' => 1
         ]);
 
         factory(\App\Question::class)->create([
-            'name' => 'Country',
+            'name' => "I currently live in...",
             'type' => 'dropdown',
             'pos' => 2
         ]);
 
         factory(\App\Question::class)->create([
-            'name' => 'Sleep',
+            'name' => "About sleep...",
             'type' => 'multiple',
             'pos' => 3
         ]);
 
         factory(\App\Question::class)->create([
-            'name' => 'Stress',
+            'name' => "About stress...",
             'type' => 'multiple',
             'pos' => 4
         ]);
 
         factory(\App\Question::class)->create([
-            'name' => 'Sickness',
+            'name' => "About sickness...",
             'type' => 'multiple',
             'pos' => 5
         ]);
 
         factory(\App\Question::class)->create([
-            'name' => 'Nutrition',
+            'name' => "About nutrition...",
             'type' => 'multiple',
             'pos' => 6
         ]);
 
         factory(\App\Question::class)->create([
-            'name' => 'Nutrition Followup',
+            'name' => 'About nutrition followup',
             'type' => 'multiple',
             'pos' => 7
         ]);
 
         factory(\App\Question::class)->create([
-            'name' => 'Exercise',
+            'name' => "About exercise...",
             'type' => 'multiple',
             'pos' => 8
         ]);
 
         factory(\App\Question::class)->create([
-            'name' => 'Exercise Followup',
+            'name' => 'About exercise followup',
             'type' => 'multiple',
             'pos' => 9
         ]);
 
         factory(\App\Question::class)->create([
-            'name' => 'Health',
+            'name' => "About my health...",
             'type' => 'multiple',
             'pos' => 10
         ]);
 
         factory(\App\Question::class)->create([
-            'name' => 'Beauty',
+            'name' => "About my skin, my main concern is...",
             'type' => 'multiple',
             'pos' => 11
         ]);
 
         factory(\App\Question::class)->create([
-            'name' => 'Freetime',
+            'name' => "About freetime...",
             'type' => 'multiple',
             'pos' => 12
         ]);
 
         factory(\App\Question::class)->create([
-            'name' => 'LOS',
+            'name' => "About length of stay...",
             'type' => 'multiple',
             'pos' => 13
         ]);
