fa1627e4b5925832fd4d051d6fea0c235f20def6
Pixlforge <celien@pixlforge.ch>
2018-03-16 11:31:43 +0100

    First commit

---
+++ resources/assets/js/app/views/ViewLastStep.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>One Last Step!</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
---
+++ resources/assets/js/app/views/ViewResults.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Results
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
