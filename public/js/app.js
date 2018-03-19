2fcccab0ce34dfc42b0843f6b7888bdee6c7527c
Pixlforge <celien@pixlforge.ch>
2018-03-19 16:42:40 +0100

    Radio button selected state updated to dark grey

--- public/js/app.js
+++ public/js/app.js
@@ -53322,21 +53322,17 @@ if (false) {
 /***/ (function(module, exports, __webpack_require__) {
 
 var disposed = false
-function injectStyle (ssrContext) {
-  if (disposed) return
-  __webpack_require__(406)
-}
 var normalizeComponent = __webpack_require__(1)
 /* script */
 var __vue_script__ = __webpack_require__(404)
 /* template */
-var __vue_template__ = __webpack_require__(408)
+var __vue_template__ = __webpack_require__(405)
 /* template functional */
 var __vue_template_functional__ = false
 /* styles */
-var __vue_styles__ = injectStyle
+var __vue_styles__ = null
 /* scopeId */
-var __vue_scopeId__ = "data-v-70bc9003"
+var __vue_scopeId__ = null
 /* moduleIdentifier (server only) */
 var __vue_module_identifier__ = null
 var Component = normalizeComponent(
@@ -53383,12 +53379,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
-//
-//
-//
-//
-//
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: {
@@ -53403,7 +53393,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
   },
   computed: {
     shouldStyle: function shouldStyle() {
-      return this.selected ? 'url(#gradient)' : 'white';
+      return this.selected ? '#4e4f4f' : 'white';
     }
   },
   methods: {
@@ -53414,48 +53404,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 });
 
 /***/ }),
-/* 405 */,
-/* 406 */
-/***/ (function(module, exports, __webpack_require__) {
-
-// style-loader: Adds some css to the DOM by adding a <style> tag
-
-// load the styles
-var content = __webpack_require__(407);
-if(typeof content === 'string') content = [[module.i, content, '']];
-if(content.locals) module.exports = content.locals;
-// add the styles to the DOM
-var update = __webpack_require__(3)("c60d7da0", content, false, {});
-// Hot Module Replacement
-if(false) {
- // When the styles change, update the <style> tags
- if(!content.locals) {
-   module.hot.accept("!!../../../../../../node_modules/css-loader/index.js!../../../../../../node_modules/vue-loader/lib/style-compiler/index.js?{\"vue\":true,\"id\":\"data-v-70bc9003\",\"scoped\":true,\"hasInlineConfig\":true}!../../../../../../node_modules/vue-loader/lib/selector.js?type=styles&index=0!./AppRadioButton.vue", function() {
-     var newContent = require("!!../../../../../../node_modules/css-loader/index.js!../../../../../../node_modules/vue-loader/lib/style-compiler/index.js?{\"vue\":true,\"id\":\"data-v-70bc9003\",\"scoped\":true,\"hasInlineConfig\":true}!../../../../../../node_modules/vue-loader/lib/selector.js?type=styles&index=0!./AppRadioButton.vue");
-     if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
-     update(newContent);
-   });
- }
- // When the module is disposed, remove the <style> tags
- module.hot.dispose(function() { update(); });
-}
-
-/***/ }),
-/* 407 */
-/***/ (function(module, exports, __webpack_require__) {
-
-exports = module.exports = __webpack_require__(2)(false);
-// imports
-
-
-// module
-exports.push([module.i, "\nsvg[data-v-70bc9003] {\n  -webkit-transform: rotate(45deg);\n          transform: rotate(45deg);\n}\n", ""]);
-
-// exports
-
-
-/***/ }),
-/* 408 */
+/* 405 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var render = function() {
@@ -53470,29 +53419,6 @@ var render = function() {
         "svg",
         { attrs: { width: "70px", height: "70px", viewBox: "0 0 70 70" } },
         [
-          _c(
-            "defs",
-            [
-              _c(
-                "linearGradient",
-                { attrs: { id: "gradient" } },
-                [
-                  _c("stop", {
-                    staticClass: "stop-color--turquoise",
-                    attrs: { offset: "20%" }
-                  }),
-                  _vm._v(" "),
-                  _c("stop", {
-                    staticClass: "stop-color--dark-grey",
-                    attrs: { offset: "100%", "stop-opacity": "0.9" }
-                  })
-                ],
-                1
-              )
-            ],
-            1
-          ),
-          _vm._v(" "),
           _c("circle", {
             attrs: { cx: "35", cy: "35", r: "25", fill: _vm.shouldStyle }
           })
--- resources/assets/js/app/components/buttons/AppRadioButton.vue
+++ resources/assets/js/app/components/buttons/AppRadioButton.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="radio__button" @click="selection">
     <svg width="70px" height="70px" viewBox="0 0 70 70">
-      <defs>
-        <linearGradient id="gradient">
-          <stop class="stop-color--turquoise" offset="20%"/>
-          <stop class="stop-color--dark-grey" offset="100%" stop-opacity="0.9"/>
-        </linearGradient>
-      </defs>
       <circle cx="35" cy="35" r="25" :fill="shouldStyle"/>
     </svg>
     <slot></slot>
@@ -27,7 +21,7 @@
     },
     computed: {
       shouldStyle() {
-        return this.selected ? 'url(#gradient)' : 'white'
+        return this.selected ? '#4e4f4f' : 'white'
       }
     },
     methods: {
@@ -37,9 +31,3 @@
     }
   }
 </script>
-
-<style scoped>
-  svg {
-    transform: rotate(45deg);
-  }
-</style>
